e9ec41ba1939de64ff6994e960233b305a5a7b93
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-17 11:11:58 +0200

    Main gameplay up and running

--- Source/Frontend/Web/App/Quiz/Game.js
+++ Source/Frontend/Web/App/Quiz/Game.js
@@ -6,23 +6,23 @@ export class Game {
         this.quiz = ko.observable({
             id: 42, title: "Magical quiz", description: "This is the awesome one", questions: [
                 {
+                    id: 43,
                     question: "Name all magicians", options: [
-                        { text: "Lord Vader", isCorrect: false },
-                        { text: "El Presidento", isCorrect: false },
-                        { text: "David Blaine", isCorrect: true },
-                        { text: "David Copperfield", isCorrect: true }
+                        { id: 44, text: "Lord Vader", isCorrect: false },
+                        { id: 45, text: "El Presidento", isCorrect: false },
+                        { id: 46, text: "David Blaine", isCorrect: true },
+                        { id: 47, text: "David Copperfield", isCorrect: true }
                     ]
                 },
                 {
+                    id: 48,
                     question: "Name all horses", options: [
-                        { text: "Horse man", isCorrect: false },
-                        { text: "El horso", isCorrect: false },
-                        { text: "David Horso", isCorrect: true },
-                        { text: "David Copperfield", isCorrect: true }
+                        { id: 48, text: "Horse man", isCorrect: false },
+                        { id: 49, text: "El horso", isCorrect: false },
+                        { id: 50, text: "David Horso", isCorrect: true },
+                        { id: 51, text: "David Copperfield", isCorrect: true }
                     ]
                 }
-                
-
             ]
         });
         //globalState.currentQuiz;
@@ -31,8 +31,25 @@ export class Game {
             return self.quiz().questions[self.currentQuestionIndex()]
         });
 
+        this.currentQuestion.subscribe(this.prepareQuestion);
+        this.prepareQuestion(this.currentQuestion());
+
+        this.currentQuestionOptions = ko.computed(() => {
+            let options = self.currentQuestion().options;
+            return options;
+        });
+
+        this.isLastQuestion = ko.computed(() => self.currentQuestionIndex() == self.quiz().questions.length-1);
         this.hasPrevious = ko.computed(() => self.currentQuestionIndex() != 0);
-        this.hasNext = ko.computed(() => self.currentQuestionIndex() < self.quiz().questions.length-1);
+        this.hasNext = ko.computed(() => self.currentQuestionIndex() < self.quiz().questions.length-1  && !self.isLastQuestion());
+    }
+
+    prepareQuestion(question) {
+        question.options.forEach(option => {
+            if( !option.selected || !ko.isObservable(option.selected) ) {
+                option.selected = ko.observable(false);
+            }
+        });
     }
 
     goToPreviousQuestion() {
@@ -42,8 +59,16 @@ export class Game {
     }
 
     goToNextQuestion() {
+        this.currentQuestion().options.forEach(option => {
+            console.log(`${option.text} - ${option.selected()}`)
+        });
+        
         if( this.currentQuestionIndex() < this.quiz().questions.length-1 ) {
             this.currentQuestionIndex(this.currentQuestionIndex()+1);
         }
+    }
+
+    submitAnswers() {
+
     }
 }
