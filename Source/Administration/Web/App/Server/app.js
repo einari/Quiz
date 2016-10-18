39f55fa6d8e3fe6472675226a47d0fcfa73c665c
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-18 11:26:22 +0200

    Removing commented code

--- Source/Administration/Web/App/Server/app.js
+++ Source/Administration/Web/App/Server/app.js
@@ -1,7 +1,7 @@
 import https from "https";
 import express from "express";
 import Quizes from "./Quizes";
-import rabbit from "rabbit.js";
+
 import bodyParser from "body-parser";
 
 
@@ -17,32 +17,6 @@ app.get("/server/*", (request, response, next) => {
 new Quizes(app);
 
 app.use(express.static(__dirname+"/../"));
-
-/*
-let context = rabbit.createContext("amqp://192.168.50.50");
-context.on("ready", () => {
-    console.log("Ready");
-    let pub = context.socket("PUB");
-    let sub = context.socket("SUB");
-
-    //sub.pipe(process.stdout);
-    sub.setEncoding("utf8");
-    sub.on("data", message => {
-        console.log(`Message : ${message}`);
-    });
-
-    sub.connect("events", () => {
-        pub.connect("events", () => {
-
-            app.get("/message", (request, response) => {
-                pub.write(JSON.stringify({weclome: `Time : ${new Date()}`}), "utf8");
-                response.status(200);
-                response.end();
-            })
-        });
-    });
-});
-*/
 
 console.log("Start Express server");
 app.listen(3001, () => {
