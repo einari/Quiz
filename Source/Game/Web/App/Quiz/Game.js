9083be8755d70cfe7d5a26008200c1255e219278
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-18 21:03:21 +0200

    Adding spinner when submitting

--- Source/Game/Web/App/Quiz/Game.js
+++ Source/Game/Web/App/Quiz/Game.js
@@ -4,38 +4,6 @@ export class Game {
     constructor() {
         let self = this;
         this.quiz = ko.observable(globalState.currentQuiz());
-        //     {
-        //     id: 42, title: "Magical quiz", description: "This is the awesome one", questions: [
-        //         {
-        //             id: 43,
-        //             question: "Name all magicians", options: [
-        //                 { id: 44, text: "Lord Vader", isCorrect: false },
-        //                 { id: 45, text: "El Presidento", isCorrect: false },
-        //                 { id: 46, text: "David Blaine", isCorrect: true },
-        //                 { id: 47, text: "David Copperfield", isCorrect: true }
-        //             ]
-        //         },
-        //         {
-        //             id: 48,
-        //             question: "Find the horse", options: [
-        //                 { id: 48, text: "Horse man", isCorrect: false },
-        //                 { id: 49, text: "El horso", isCorrect: false },
-        //                 { id: 50, text: "David Horso", isCorrect: true },
-        //                 { id: 51, text: "David Copperfield", isCorrect: false }
-        //             ]
-        //         },
-        //         {
-        //             id: 52,
-        //             question: "Check all that applies", options: [
-        //                 { id: 53, text: "Water is heavy", isCorrect: false },
-        //                 { id: 54, text: "Water is fluid", isCorrect: true },
-        //                 { id: 55, text: "Water is transparent", isCorrect: true },
-        //                 { id: 56, text: "Water is solid", isCorrect: false }
-        //             ]
-        //         },
-        //     ]
-        // });
-        //;
         this.currentQuestionIndex = ko.observable(0);
         this.currentQuestion = ko.computed(() => {
             return self.quiz().questions[self.currentQuestionIndex()]
@@ -62,6 +30,8 @@ export class Game {
         this.isLastQuestion = ko.computed(() => self.currentQuestionIndex() == self.quiz().questions.length-1);
         this.hasPrevious = ko.computed(() => self.currentQuestionIndex() != 0);
         this.hasNext = ko.computed(() => self.currentQuestionIndex() < self.quiz().questions.length-1  && !self.isLastQuestion());
+
+        this.submitting = ko.observable(false);
     }
 
     prepareQuestion(question) {
@@ -90,6 +60,8 @@ export class Game {
 
     submitAnswers() {
         let questionsAndAnswers = [];
+
+        this.submitting(true);
 
         this.quiz().questions.forEach(question => {
             let questionAndAnswers = {
