b15055ada0c40a794fe37ab27d97b0f910a0b58f
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-18 12:34:10 +0200

    Moved game to a higher level

--- Source/Backend/build.sh
+++ Source/Backend/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-DIR=`dirname $0`
-dotnet restore $DIR/../Backend/src/Backend/Game/project.json -s ~/ServiceFabricSDK/csharp/packages -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
-dotnet build $DIR/../Backend/src/Backend/Game/project.json
-dotnet publish $DIR/../Backend/src/Backend/Game/project.json -o $DIR/../Backend/Backend/GamePkg/Code
+dotnet restore ./Game/project.json -s ~/ServiceFabricSDK/csharp/packages -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
+dotnet build ./Game/project.json
+dotnet publish ./Game/project.json -o ./QuizBackend/GamePkg/Code
