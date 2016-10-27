7a75e37153ece7d5ad0aec95e5f7ca0fa48f641c
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-27 15:09:59 +0200

    Forgot to $ prefix env variable

--- Source/pushDockerImages.sh
+++ Source/pushDockerImages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push DOCKER_TARGETACCOUNT/quizadministrationweb
-docker push DOCKER_TARGETACCOUNT/quizgameweb
+docker push $DOCKER_TARGETACCOUNT/quizadministrationweb
+docker push $DOCKER_TARGETACCOUNT/quizgameweb
