7036383af27326e807ac281fb09eb51a2e6539ce
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-28 11:27:09 +0200

    Bringing back the real code.. :)

--- Source/Game/Web/App/Server/app.js
+++ Source/Game/Web/App/Server/app.js
@@ -6,12 +6,15 @@ import Attempts from "./Attempts";
 import bodyParser from "body-parser";
 import socketio from "socket.io";
 import {quizMessages} from "./QuizMessages";
+import {namingService} from "./NamingService"
 
 let app = express();
 let server = http.Server(app);
 
+
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+
 
 app.get("/server/*", (request, response, next) => {
     response.status(404);
@@ -19,25 +22,25 @@ app.get("/server/*", (request, response, next) => {
 });
 
 
+/*
 app.get("/env/", (request, response, next) => {
     response.send(process.env);
     response.end();
 });
+*/
 
-/*
 new Quizes(app);
 new Attempts(app);
-*/
 
 app.use(express.static(__dirname+"/../"));
 
-/*
+
 let io = socketio(server);
 io.on("connection", socket => {
     quizMessages.quizAdded.subscribe(quiz => socket.emit("quizAdded", quiz));
     quizMessages.attemptScored.subscribe(result => socket.emit("attemptScored", result));
 });
-*/
+
 
 console.log("Start server");
 server.listen(3000, () => {
