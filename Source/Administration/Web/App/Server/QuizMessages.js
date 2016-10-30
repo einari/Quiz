093b58f6009b753f26dac5b7781b56e1886bb8c5
Einar Ingebrigtsen <einar@dolittle.com>
2016-10-30 21:41:02 +0100

    Resolving RabbitMQ endpoint

--- Source/Administration/Web/App/Server/QuizMessages.js
+++ Source/Administration/Web/App/Server/QuizMessages.js
@@ -1,18 +1,19 @@
 import amqp from "amqplib/callback_api";
 
-class QuizMessages
-{
+class QuizMessages {
     publish(messageType, data) {
         let message = {
             type: messageType,
             body: data
         };
 
-        amqp.connect("amqp://192.168.50.50",(error, connection) => {
-            let channel = connection.createChannel((e,channel) => {
-                let exchangeName = "events";
-                channel.assertExchange(exchangeName, "fanout", { durable: false });
-                channel.publish(exchangeName, "", new Buffer(JSON.stringify(message)));
+        namingService.resolveEndpoint("QuizCommon", "QuizCommon/Messaging", "MessagingPort").then(endpoint => {
+            amqp.connect(`amqp://${endpoint}`, (error, connection) => {
+                let channel = connection.createChannel((e, channel) => {
+                    let exchangeName = "events";
+                    channel.assertExchange(exchangeName, "fanout", { durable: false });
+                    channel.publish(exchangeName, "", new Buffer(JSON.stringify(message)));
+                });
             });
         });
     }
